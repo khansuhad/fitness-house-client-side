e422f9a12f986e83b169ed90362ccddda00fba36
khan suhad <suhadahmodkhan@gmail.com>
2023-11-29 01:52:31 +0600

    added classes section in home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,21 @@
+import { Helmet } from "react-helmet";
 import Forums from "../Forum/Forums";
 import About from "./About";
 import HomeBanner from "./HomeBanner";
 import NewsLetter from "./NewsLetter";
 import Trainer from "./Trainer";
+import AllClasses from "../Classes/AllClasses";
 
 
 const Home = () => {
     return (
         <div>
+                   <Helmet>
+            <title> Fitness house | Home </title>
+          </Helmet>
             <HomeBanner/>
             <About/>
+            <AllClasses/>
             <Forums/>
             <Trainer/>
             <NewsLetter/>
